ab2ba6726434068f354454102ff7be4a382ce75f
Radevich <n.radievich@mail.ru>
2020-10-08 19:47:16 +0300

    feat: att task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = [];
+    let finalResult = [];
+    let arr = expr.split('**********');
+    arr.forEach(item => {
+        item = item.split('');
+        for (let i = 0; i < item.length; i = i + 10) {
+
+            let num = item.slice(i, i + 10);
+
+            for (let j = 0; j < num.length; j + 2) {
+                let twoNum = num.slice(j, j = j + 2).join('');
+
+                if (twoNum === '10') {
+                    result.push('.');
+                }
+                if (twoNum === '11') {
+                    result.push('-');
+                }
+                
+            }
+            result.push('*');
+        }
+            result.push(' ');   
+    })
+    result = result.join('').trim().split(' ')
+
+    result.forEach(item => {
+        let word = '';
+        item = item.split('*');
+        
+        for (let i = 0; i< item.length; i++) {
+            for (let key in MORSE_TABLE) {
+            if (key === item[i]) {
+                word += `${MORSE_TABLE[key]}`
+            }
+        }
+        }
+        finalResult.push(word);
+    })
+        
+        return finalResult.join(' ');
+    
 }
 
 module.exports = {
